f25227ffca088b14371e8fd8b49c802dc1349842
Nuynaselr <nikita_parenskii@mail.ru>
2019-05-15 21:56:10 +0400

    add commit

--- OC/Lab_4/GiveMeArg.py
+++ OC/Lab_4/GiveMeArg.py
@@ -5,15 +5,20 @@ import os
 
 class CheckArg:
     def __init__(self, arg):
-        self.buffer_size = 256
-        self.arg = arg
+        self.buffer_size = 256  # Create buffer
+        self.arg = arg  # write arguments in class object
 
+    # Implements checks
+    #
+    # If a command is entered that is in the command list,
+    # then the corresponding group is called.
+    #
     def start(self):
         if len(self.arg) == 2 and self.arg[1] == '--help':
             self.help()
 
         elif self.arg[1] == 'info':
-            word = self.arg[2] if len(self.arg) == 3 else '.'
+            word = self.arg[3] if len(self.arg) == 4 else '.'
             if self.chekcMe(str(self.arg[2])):
                 self.info(self.arg[2], word)
             else:
@@ -28,6 +33,10 @@ class CheckArg:
         else:
             print('Unknown command. Write "python GiveMeArg.py --help"')
 
+    #  Open two files
+    #  Original file and copy file
+    #  And using the buffer, copy data between files
+    #  Start with end file and copy before start file
     def cp(self):
         src = self.arg[2]
         dst = self.arg[3]
@@ -40,20 +49,29 @@ class CheckArg:
                     small_buff = sender.read(self.buffer_size)
                     receiver.write(small_buff)
 
+    #  Call function 'call' with arguments
+    #  ls - information on directory
+    #  arguments - arguments for 'ls'
+    #  file - if there is a file or a specific directory,
+    #  then information about them is displayed, otherwise about the current
     def info(self, arguments, file):
         try:
             call(['ls', arguments, file])
         except Exception as ex:
             print(ex)
 
+    #  converts permissions from decimal to octal and changes file permissions
     def chmod(self):
         rights = int(self.arg[3], 8)
         os.chmod(self.arg[2], rights)
 
+    #  open file 'help.txt'
+    #  which stores reference information
     def help(self):
         with open('help.txt', 'r') as file:
             print(file.read())
 
+    #  checks the arguments for function 'info'
     def chekcMe(self, argument):
         row = '-lahf'
         for word in argument:
@@ -63,5 +81,5 @@ class CheckArg:
 
 
 if __name__ == '__main__':
-    test = CheckArg(argv)
-    test.start()
+    test = CheckArg(argv)  # Create object class CheckArg and send arg in object class
+    test.start()  # Start programm
