cd14e4104cb959c904d79e40c2b532fa3b535223
Nuynaselr <36643759+Nuynaselr@users.noreply.github.com>
2019-07-16 03:17:51 +0400

    Update VidInImg.py

--- SummerPractice/VidInImg.py
+++ SummerPractice/VidInImg.py
@@ -39,58 +39,47 @@ def cubic_polynomial(a, b, c, d, x):
 
 def rotate_bicubic(image, angle):
     source_image = image
-
+    
     angle_rad = math.radians(angle)
     height, width, chanel = source_image.shape
-
     dest_image = numpy.ones((height, width, 3), numpy.uint8)
-
     half_width = width // 2
     half_height = height // 2
-
+    
     radius = math.sqrt(half_height * half_height + half_width * half_width)
     const_b = math.atan2(half_height, half_width)
 
     cos = math.cos(angle_rad)
     sin = math.sin(angle_rad)
-
     rcos = radius * math.cos(const_b)
     rsin = radius * math.sin(const_b)
-
     for i in range(-half_height, half_height):
         for j in range(-half_width, half_width):
             x = i * cos - j * sin + rsin
             y = i * sin + j * cos + rcos
-
             x_fract = x - int(x)
             y_fract = y - int(y)
-
             x = int(x)
             y = int(y)
 
             pixel = numpy.ones(3, numpy.uint8)
-
             if (height > x > 0) and (width > y > 0):
                 c00 = source_image[max(x - 1, 0), max(y - 1, 0)]
                 c10 = source_image[x, max(y - 1, 0)]
                 c20 = source_image[min(x + 1, height - 1), max(y - 1, 0)]
                 c30 = source_image[min(x + 2, height - 1), max(y - 1, 0)]
-
                 c01 = source_image[max(x - 1, 0), y]
                 c11 = source_image[x, y]
                 c21 = source_image[min(x + 1, height - 1), y]
                 c31 = source_image[min(x + 2, height - 1), y]
-
                 c02 = source_image[max(x - 1, 0), min(y + 1, width - 1)]
                 c12 = source_image[x, min(y + 1, width - 1)]
                 c22 = source_image[min(x + 1, height - 1), min(y + 1, width - 1)]
                 c32 = source_image[min(x + 2, height - 1), min(y + 1, width - 1)]
-
                 c03 = source_image[max(x - 1, 0), min(y + 2, width - 1)]
                 c13 = source_image[x, min(y + 2, width - 1)]
                 c23 = source_image[min(x + 1, height - 1), min(y + 2, width - 1)]
                 c33 = source_image[min(x + 2, height - 1), min(y + 2, width - 1)]
-
                 for coordinate in range(3):
                     c0 = cubic_polynomial(c00[coordinate], c10[coordinate], c20[coordinate], c30[coordinate], x_fract)
                     c1 = cubic_polynomial(c01[coordinate], c11[coordinate], c21[coordinate], c31[coordinate], x_fract)
@@ -102,11 +91,7 @@ def rotate_bicubic(image, angle):
                         pixel[coordinate] = 255
                     elif pixel[coordinate] < 0:
                         pixel[coordinate] = 0
-
                 dest_image[i + half_height][j + half_width] = pixel
-
-        print('i - ', i, ' j - ', j)
-
     cv2.imwrite('frame' + str(number_frame) + extension_image, dest_image)
 
 
